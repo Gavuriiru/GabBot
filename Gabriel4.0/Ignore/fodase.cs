9bf2d529c48a1e2ed3a39896ab7fd665d53ec0f9
JoseGabrielRP <58956441+JoseGabrielRP@users.noreply.github.com>
2020-06-09 21:04:04 -0300

    Putariaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
    FOOOOOOOOOOOOOOOOOOOOOOOOOOODA-SE

--- Gabriel4.0/Ignore/fodase.cs
+++ Gabriel4.0/Ignore/fodase.cs
@@ -3,6 +3,6 @@
 
     class Fodase
     {
-        public static readonly string[] Links = { "https://i.imgur.com/JpXoHB8.jpg", "https://i.imgur.com/ZJWApEe.jpg", "https://i.imgur.com/CV4vFOh.jpg", "https://i.imgur.com/CV4vFOh.jpg", "https://i.imgur.com/cVwH6OY.jpg", "https://i.imgur.com/tvvJTdo.jpg", "https://i.imgur.com/gnwa8pu.jpg", "https://i.imgur.com/cpisPQr.jpg", "https://i.imgur.com/XqV5WKz.jpg" };
+        public static readonly string[] Links = { "https://i.imgur.com/JpXoHB8.jpg", "https://i.imgur.com/ZJWApEe.jpg", "https://i.imgur.com/CV4vFOh.jpg", "https://i.imgur.com/KMmpTud.png", "https://i.imgur.com/CV4vFOh.jpg", "https://i.imgur.com/cVwH6OY.jpg", "https://i.imgur.com/tvvJTdo.jpg", "https://i.imgur.com/gnwa8pu.jpg", "https://i.imgur.com/cpisPQr.jpg", "https://i.imgur.com/XqV5WKz.jpg" };
     }
 }
